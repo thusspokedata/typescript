10029e4b355489c76e6e567f311cd915cb6b3eb2
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-08-23 00:46:48 +0200

    add enums, never, tuples, void

---
+++ bases/tipos/enums.ts
@@ -0,0 +1,11 @@
+(() => {
+  enum AudioLevel {
+    min,
+    medium,
+    max,
+  }
+  let currentAudio = AudioLevel.medium;
+
+  console.log(AudioLevel);
+  console.log(currentAudio);
+})();
---
+++ bases/tipos/void.ts
@@ -0,0 +1,11 @@
+(() => {
+  function callBatman(): void {
+    return;
+  }
+
+  const callSuperman = (): void => {
+    return;
+  };
+  const a = callBatman();
+  console.log(a);
+})();
